7f71de06e9e3f79103d48a4ee69269f78c3bdedf
Panagiotis Kordas <panagiotiskordas92@gmail.com>
2022-12-26 15:12:48 +0200

    hardcoded productd added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
-
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
-      <header>Hey</header>
+      <header><a href="/">Panelpo</a></header>
+      <main>
+        <h1>Main Products</h1>
+        <div className="products">
+          {data.products.map(product=>{
+              return (
+                <div className='product' key={product.sku}>
+                    <a href={`/product/${product.sku}`}>
+                      <img src={product.image} alt={product.name}></img>
+                    </a>
+                    
+                    <div className="product-info">
+                      <a href={`/product/${product.sku}`}>
+                        <p>{product.name}</p>
+                      </a>
+                      <strong>&euro; {product.price}</strong>
+                    </div>
+                </div>
+              );
+          })}
+        </div>
+      </main>
     </div>
   );
 }
